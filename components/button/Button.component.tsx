d9ffe2c80f93e7730a2d49a24ba7514ee6f4f675
Florian <mesagouiffes@gmail.com>
2021-04-19 08:18:22 +0200

    :sparkles: Image in Button component

--- components/button/Button.component.tsx
+++ components/button/Button.component.tsx
@@ -2,16 +2,41 @@ import React from 'react'
 
 import style from './Button.module.scss'
 
+type Image = {
+  src: string
+  alt: string
+}
+
 type ButtonProps = {
   isInput?: boolean
+  onClick: () => void
+  image?: Image
   value: string
 }
 
-const Button: React.FC<ButtonProps> = ({ isInput = false, value }) => {
+const Button: React.FC<ButtonProps> = ({
+  isInput = false,
+  value,
+  onClick,
+  image,
+}) => {
   return isInput ? (
-    <input type="button" value={value} className={style['input-button']} />
+    <div className={style.container}>
+      {image !== undefined && <img src={image.src} alt={image.alt} />}
+      <input
+        onClick={onClick}
+        type="button"
+        value={value}
+        className={style['input-button']}
+      />
+    </div>
   ) : (
-    <button className={style.button}>{value}</button>
+    <div className={style.container}>
+      {image && <img src={image.src} alt={image.alt} />}
+      <button onClick={onClick} className={style.button}>
+        {value}
+      </button>
+    </div>
   )
 }
 
